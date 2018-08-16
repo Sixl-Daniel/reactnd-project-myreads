4cf3ca21917d34d16803616a36247896b6fc70d3
Sixl-Daniel <sixl.daniel@googlemail.com>
2018-08-16 13:48:31 +0200

    feat: Add noopener/noreferrer to external link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
                 <Container textAlign='center' id='footer-content'>
                     <Image centered size='mini' src='/logo-book.svg' />
                     <Header inverted as='h3'>MyReads – A Book Tracking App</Header>
-                    <p><a href='https://github.com/Sixl-Daniel/reactnd-project-myreads' target='_blank'><Icon name='github' /> Show project on Github</a></p>
+                    <p><a href='https://github.com/Sixl-Daniel/reactnd-project-myreads' target='_blank' rel="noopener noreferrer"><Icon name='github' /> Show project on Github</a></p>
                     <p>&copy; 2018 Sixl</p>
                 </Container>
             </Segment>
