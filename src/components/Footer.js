7d30f029cc8f4043ec6fc80138ea309b2bd943cb
Sixl-Daniel <sixl.daniel@googlemail.com>
2018-08-16 13:31:01 +0200

    feat: Add footer link to GitHub

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Image, Segment } from 'semantic-ui-react';
+import { Container, Header, Icon, Image, Segment } from 'semantic-ui-react';
 
 class Footer extends Component {
     render() {
@@ -7,6 +7,8 @@ class Footer extends Component {
             <Segment as='footer' vertical inverted id='footer-wrapper'>
                 <Container textAlign='center' id='footer-content'>
                     <Image centered size='mini' src='/logo-book.svg' />
+                    <Header inverted as='h3'>MyReads – A Book Tracking App</Header>
+                    <p><a href='https://github.com/Sixl-Daniel/reactnd-project-myreads' target='_blank'><Icon name='github' /> Show project on Github</a></p>
                     <p>&copy; 2018 Sixl</p>
                 </Container>
             </Segment>
